95613914885298697563e24c6296371213b17f4e
hirohiro1211 <72436139+hirohiro1211@users.noreply.github.com>
2020-11-09 18:24:24 +0900

    修正２

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :item do
-    name             { Faker::Name.initials(number: 40) }
-    introduce        { 'この商品は' }
-    category
-    delivery_burden
-    delivery_date
-    prefecture
-    state
-    price { 10_000 }
+    name                { Faker::Name.initials(number: 40) }
+    introduce           { 'この商品は' }
+    category_id         { 2 }
+    delivery_burden_id  { 2 }
+    delivery_date_id    { 2 }
+    prefecture_id       { 4 }
+    state_id            { 3 }
+    price            { 10000 }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,38 +21,38 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Introduce can't be blank")
     end
-    it 'カテゴリー必須' do
-      @item.category = nil
+    it 'カテゴリーの選択肢が「１」だと出品できない' do
+      @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category is not a number")
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
-    it '商品の状態についての情報が必須' do
-      @item.state = nil
+    it '商品の状態の選択肢が「１」だと出品できない' do
+      @item.state_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("State is not a number")
+      expect(@item.errors.full_messages).to include("State must be other than 1")
     end
-    it '配送料の負担についての情報が必須' do
-      @item.delivery_burden = nil
+    it '配送料の負担の選択肢が「１」だと出品できない' do
+      @item.delivery_burden_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Delivery burden is not a number")
+      expect(@item.errors.full_messages).to include("Delivery burden must be other than 1")
     end
-    it '発送元の地域についての情報が必須' do
-      @item.prefecture = nil
+    it '発送元の地域の選択肢が「０」だと出品できない' do
+      @item.prefecture_id = 0
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
     end
-    it '発送までの日数についての情報が必須' do
-      @item.delivery_date = nil
+    it '発送までの日数選択肢が「１」だと出品できない' do
+      @item.delivery_date_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include("Delivery date is not a number")
+      expect(@item.errors.full_messages).to include("Delivery date must be other than 1")
     end
     it 'priceが300円未満では保存できない' do
-      @item.price = '100'
+      @item.price = 100
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it 'priceが9999999以上では保存できない' do
-      @item.price = '100000000'
+      @item.price = 100000000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
